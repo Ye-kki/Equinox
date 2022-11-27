fdd56e894b46cfa9680126ea93368c6880729a01
Ye-kki <yechan_choi@naver.com>
2022-11-27 21:54:44 +0900

    REFACTOR: 코드 리팩토링 #20

--- Equinox_app/script.js
+++ Equinox_app/script.js
@@ -2,7 +2,7 @@ let Red, Green, Blue;
 let brightness;
 
 function kelvinToRGB(Temperature) {
-Temperature = Temperature / 100
+  Temperature = Temperature / 100
 
   if(Temperature <= 66) Red = 255;
   else {
@@ -51,7 +51,7 @@ var kelvin = new iro.ColorPicker(".kelvin", {
         }
       },
     ]
-  });
+});
 
 var value = new iro.ColorPicker(".value", {
     width: 315,
@@ -67,7 +67,7 @@ var value = new iro.ColorPicker(".value", {
         }
       },
     ]
-  });
+});
 
 const background = document.querySelector(".background");
 const blurCircle = document.querySelector(".blur-circle");
@@ -76,9 +76,7 @@ const lightCircle = document.querySelector(".light-circle");
 kelvin.on(["color:init", "color:change"], function (color) {
     // console.log('K' + parseInt(color.kelvin));
     kelvinToRGB(color.kelvin);
-    console.log(Red);
-    console.log(Green);
-    console.log(Blue);
+    console.log(parseInt(Red),parseInt(Green),parseInt(Blue));
 
     gsap.to(background, 0.7, {
       background: "linear-gradient(179.99deg, rgba(0, 0, 0, 0.9) -9.21%, rgba(" + Red + ', ' + Green + ', ' + Blue + ',' + brightness/200 + ") 37%, rgba(0, 0, 0, 0.9) 76.28%)",
@@ -108,6 +106,7 @@ value.on(["color:init", "color:change"], function (color1) {
 const leftBtn = document.querySelector(".left-btn");
 const rightBtn = document.querySelector(".right-btn");
 const colorOption = document.querySelector(".color-option");
+let sunLoop;
 
 leftBtn.addEventListener('click', function () {
   gsap.to(blurCircle, 0, {
@@ -125,7 +124,7 @@ leftBtn.addEventListener('click', function () {
   gsap.to(sun, 0, {
     display: "none",
   });
-  clearInterval(loop);
+  clearInterval(sunLoop);
 });
 
 rightBtn.addEventListener('click', function () {
@@ -144,63 +143,62 @@ rightBtn.addEventListener('click', function () {
   gsap.to(sun, 0, {
     display: "block",
   });
-  clearInterval(loop);
-  loop = setInterval(update, 2000);
+  clearInterval(sunLoop);
+  sunLoop = setInterval(update, 5*1000);
 });
 
-let time = new Date();
-let year = time.getFullYear(); // 년도
-let month = time.getMonth() + 1;  // 월
-let date = time.getDate();  // 날짜   
-let hours = time.getHours(); // 시
-let minutes = time.getMinutes();  // 분
-let today = String(year) + String(month) + String(date);
-let sunrise, sunset;
-let sunriseMin, sunrsetMin;
-let now = hours*60 + minutes;
+let time, year, month, date, hours, minutes, today
+let sunrise, sunset, sunriseMin, sunsetMin;
+let now;
 
-var xhr = new XMLHttpRequest();
-var url = 'http://apis.data.go.kr/B090041/openapi/service/RiseSetInfoService/getAreaRiseSetInfo'; /*URL*/
-var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'sz%2BT0LU2UAtWcd3lRFWm8FX1EhVcy51Dd77mxJe%2FxyaZdi4uKvJzBg12sZm3dlpsl9p6soutm8O13bdAKeamFQ%3D%3D'; /*Service Key*/
-queryParams += '&' + encodeURIComponent('locdate') + '=' + encodeURIComponent(today); /**/
-queryParams += '&' + encodeURIComponent('location') + '=' + encodeURIComponent('서울'); /**/
-xhr.open('GET', url + queryParams);
-xhr.onreadystatechange = function () {
-    if (this.readyState == 4) {
-        // alert('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
-        let xmlParser = new DOMParser();
-        let xmlDoc = xmlParser.parseFromString(xhr.responseText, "text/xml"); // parseFromString() 메소드를 이용해 문자열을 파싱함. 
-        sunrise = xmlDoc.getElementsByTagName("sunrise")[0].textContent; 
-        sunset = xmlDoc.getElementsByTagName("sunset")[0].textContent; 
-        console.log(sunrise);
-        console.log(sunset);
-        sunriseMin = (parseInt(sunrise[0]*10) + parseInt(sunrise[1])) * 60 + parseInt(sunrise[2]*10) + parseInt(sunrise[3]);
-        sunsetMin = (parseInt(sunset[0]*10) + parseInt(sunset[1])) * 60 + parseInt(sunset[2]*10) + parseInt(sunset[3]);
-        document.getElementById('sunrise-time').textContent= "AM " + sunrise[0] + sunrise[1] + ':' + sunrise[2] + sunrise[3];
-        document.getElementById('sunset-time').textContent= "PM " + sunset[0] + sunset[1] + ':' + sunset[2] + sunset[3];
-      }
-};
+function sunRequest (){
+  var xhr = new XMLHttpRequest();
+  var url = 'http://apis.data.go.kr/B090041/openapi/service/RiseSetInfoService/getAreaRiseSetInfo'; /*URL*/
+  var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'sz%2BT0LU2UAtWcd3lRFWm8FX1EhVcy51Dd77mxJe%2FxyaZdi4uKvJzBg12sZm3dlpsl9p6soutm8O13bdAKeamFQ%3D%3D'; /*Service Key*/
+  queryParams += '&' + encodeURIComponent('locdate') + '=' + encodeURIComponent(today); /**/
+  queryParams += '&' + encodeURIComponent('location') + '=' + encodeURIComponent('서울'); /**/
+  xhr.open('GET', url + queryParams);
+  xhr.onreadystatechange = function () {
+      if (this.readyState == 4) {
+          // alert('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
+          let xmlParser = new DOMParser();
+          let xmlDoc = xmlParser.parseFromString(xhr.responseText, "text/xml"); // parseFromString() 메소드를 이용해 문자열을 파싱함. 
+          sunrise = xmlDoc.getElementsByTagName("sunrise")[0].textContent; 
+          sunset = xmlDoc.getElementsByTagName("sunset")[0].textContent; 
+          sunriseMin = (parseInt(sunrise[0]*10) + parseInt(sunrise[1])) * 60 + parseInt(sunrise[2]*10) + parseInt(sunrise[3]);
+          sunsetMin = (parseInt(sunset[0]*10) + parseInt(sunset[1])) * 60 + parseInt(sunset[2]*10) + parseInt(sunset[3]);
+          document.getElementById('sunrise-time').textContent= "AM " + sunrise[0] + sunrise[1] + ':' + sunrise[2] + sunrise[3];
+          document.getElementById('sunset-time').textContent= "PM " + sunset[0] + sunset[1] + ':' + sunset[2] + sunset[3];
+        }
+  };
+  xhr.send('');
+}
 
-xhr.send('');
+window.onload = () => {
+  time = new Date();
+  year = time.getFullYear(); // 년도
+  month = time.getMonth() + 1;  // 월
+  date = time.getDate();  // 날짜
+  today = String(year) + String(month) + String(date);
+  sunRequest();
+  setInterval(sunRequest, 600*1000);
+}
 
 const canvas = document.getElementById('sun');
 canvas.setAttribute("width", 390);
 canvas.setAttribute("height", 390);
-// 2. 2d모드의 그리기 객체 취득
+
 const context  = canvas.getContext("2d");
 
-var currentDegree = 0; // 태양에 대한 지구의 위치(각도)
-
-let loop; // 20FPS 프레임 속도
+var currentDegree = 0;
 
 function update() {
-  // 이전에 그림을 지우고 다시 그림
-  context.clearRect(0, 0, canvas.width, canvas.height);
-
   time = new Date();
   hours = time.getHours(); // 시
-  minutes = 8;  // 분
+  minutes = time.getMinutes();  // 분
   now = hours*60 + minutes;
+  console.log(1);
+  context.clearRect(0, 0, canvas.width, canvas.height);
 
   if(hours < 10) hours = String('0' + hours);
   if(minutes < 10) minutes = String('0' + minutes);
@@ -209,31 +207,6 @@ function update() {
     document.getElementById('ampm').textContent= 'PM';
   }
   else document.getElementById('ampm').textContent= 'AM';
-  
-  console.log(currentDegree, now);
-  drawSun();
-}
-
-function drawSun() {
-  var distanceFromSun = 160;
-
-  context.beginPath();
-
-  // 캔버스의 중간지점 + 삼각비에 길이를 곱해주면 된다. 
-  var dx = (canvas.width / 2) + Math.cos(deg2Rad(currentDegree)) * distanceFromSun;
-  var dy = (canvas.height / 2) + Math.sin(deg2Rad(currentDegree)) * distanceFromSun;
-
-  var radius = 37.5/2; 
-
-  context.arc(dx, dy, radius, 0, 2 * Math.PI);
-  context.lineWidth = 5;
-  context.fillStyle = "#FFFFFF";
-  context.fill();
-  context.shadowOffsetX = 0;
-  context.shadowOffsetY = 0;
-  context.shadowBlur = 20;
-  context.shadowColor = "#FF7A00";
-  
 
   if(now >= sunriseMin && now <= sunsetMin){
     currentDegree = 180 + ((now-sunriseMin)/(sunsetMin-sunriseMin))*180;
@@ -244,6 +217,24 @@ function drawSun() {
   else {
     currentDegree = now/sunriseMin * 90;
   }
+  drawSun();
+}
+
+function drawSun() {
+  var distanceFromSun = 160;
+
+  context.beginPath();
+
+  var dx = (canvas.width / 2) + Math.cos(deg2Rad(currentDegree)) * distanceFromSun;
+  var dy = (canvas.height / 2) + Math.sin(deg2Rad(currentDegree)) * distanceFromSun;
+
+  var radius = 37.5/2; 
+
+  context.arc(dx, dy, radius, 0, 2 * Math.PI);
+  context.fillStyle = "#FFFFFF";
+  context.shadowBlur = 20;
+  context.shadowColor = "#FF7A00";
+  context.fill();
 }
 
 function deg2Rad(degree) {
