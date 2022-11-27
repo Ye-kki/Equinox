fb6cdbefc17fa0e363e9e49836e67262159a0639
Ye-kki <yechan_choi@naver.com>
2022-11-27 18:10:41 +0900

    Feat: 일출/일몰 형태 구현

--- Equinox_app/script.js
+++ Equinox_app/script.js
@@ -94,6 +94,7 @@ value.on(["color:init", "color:change"], function (color1) {
     console.log('B' + parseInt(color1.value/100*255));
 });
 
+
 const leftBtn = document.querySelector(".left-btn");
 const rightBtn = document.querySelector(".right-btn");
 const colorOption = document.querySelector(".color-option");
@@ -108,6 +109,13 @@ leftBtn.addEventListener('click', function () {
   gsap.to(colorOption, 0.2, {
     right: "0",
   });
+  gsap.to(background, 0.2, {
+    background: "linear-gradient(179.99deg, rgba(0, 0, 0, 0.9) -9.21%, rgba(" + Red + ', ' + Green + ', ' + Blue + ", 0.3) 37%, rgba(0, 0, 0, 0.9) 76.28%)",
+  });
+  gsap.to(sun, 0, {
+    display: "none",
+  });
+  clearInterval(loop);
 });
 
 rightBtn.addEventListener('click', function () {
@@ -120,4 +128,110 @@ rightBtn.addEventListener('click', function () {
   gsap.to(colorOption, 0.2, {
     right: "100%",
   });
+  gsap.to(background, 0.2, {
+    background: "linear-gradient(179.99deg, rgba(0, 0, 0, 0.9) -9.21%, rgba(255, 255, 255, 0.9) 37%, rgba(0, 0, 0, 0.9) 76.28%)",
+  });
+  gsap.to(sun, 0, {
+    display: "block",
+  });
+  clearInterval(loop);
+  loop = setInterval(update, 2000);
 });
+
+let time = new Date();
+let year = time.getFullYear(); // 년도
+let month = time.getMonth() + 1;  // 월
+let date = time.getDate();  // 날짜   
+let hours = time.getHours(); // 시
+let minutes = time.getMinutes();  // 분
+let today = String(year) + String(month) + String(date);
+let sunrise, sunset;
+let sunriseMin, sunrsetMin;
+let now = hours*60 + minutes;
+
+var xhr = new XMLHttpRequest();
+var url = 'http://apis.data.go.kr/B090041/openapi/service/RiseSetInfoService/getAreaRiseSetInfo'; /*URL*/
+var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'sz%2BT0LU2UAtWcd3lRFWm8FX1EhVcy51Dd77mxJe%2FxyaZdi4uKvJzBg12sZm3dlpsl9p6soutm8O13bdAKeamFQ%3D%3D'; /*Service Key*/
+queryParams += '&' + encodeURIComponent('locdate') + '=' + encodeURIComponent(today); /**/
+queryParams += '&' + encodeURIComponent('location') + '=' + encodeURIComponent('서울'); /**/
+xhr.open('GET', url + queryParams);
+xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+        // alert('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
+        let xmlParser = new DOMParser();
+        let xmlDoc = xmlParser.parseFromString(xhr.responseText, "text/xml"); // parseFromString() 메소드를 이용해 문자열을 파싱함. 
+        sunrise = xmlDoc.getElementsByTagName("sunrise")[0].textContent; 
+        sunset = xmlDoc.getElementsByTagName("sunset")[0].textContent; 
+        console.log(sunrise);
+        console.log(sunset);
+        sunriseMin = (parseInt(sunrise[0]*10) + parseInt(sunrise[1])) * 60 + parseInt(sunrise[2]*10) + parseInt(sunrise[3]);
+        sunsetMin = (parseInt(sunset[0]*10) + parseInt(sunset[1])) * 60 + parseInt(sunset[2]*10) + parseInt(sunset[3]);
+      }
+};
+
+xhr.send('');
+
+const canvas = document.getElementById('sun');
+canvas.setAttribute("width", 390);
+canvas.setAttribute("height", 390);
+// 2. 2d모드의 그리기 객체 취득
+const context  = canvas.getContext("2d");
+
+var currentDegree = 0; // 태양에 대한 지구의 위치(각도)
+
+let loop; // 20FPS 프레임 속도
+
+function update() {
+  // 이전에 그림을 지우고 다시 그림
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  time = new Date();
+  year = time.getFullYear(); // 년도
+  month = time.getMonth() + 1;  // 월
+  date = time.getDate();  // 날짜   
+  hours = time.getHours(); // 시
+  minutes = time.getMinutes();  // 분
+  today = String(year) + String(month) + String(date);
+  sunrise, sunset;
+  sunriseMin, sunrsetMin;
+  now = hours*60 + minutes;
+
+  console.log(currentDegree, now);
+  drawSun();
+}
+
+function drawSun() {
+  var distanceFromSun = 160;
+
+  context.beginPath();
+
+  // 캔버스의 중간지점 + 삼각비에 길이를 곱해주면 된다. 
+  var dx = (canvas.width / 2) + Math.cos(deg2Rad(currentDegree)) * distanceFromSun;
+  var dy = (canvas.height / 2) + Math.sin(deg2Rad(currentDegree)) * distanceFromSun;
+
+  var radius = 37.5/2; 
+
+  context.arc(dx, dy, radius, 0, 2 * Math.PI);
+  context.lineWidth = 5;
+  context.fillStyle = "#FFFFFF";
+  context.fill();
+  context.shadowOffsetX = 0;
+  context.shadowOffsetY = 0;
+  context.shadowBlur = 20;
+  context.shadowColor = "#FF7A00";
+  
+
+  if(now >= sunriseMin && now <= sunsetMin){
+    currentDegree = 180 + ((now-sunriseMin)/(sunsetMin-sunriseMin))*180;
+  }
+  else if(now >=sunsetMin && now < 1440 ){
+    currentDegree =(now - sunsetMin)/(1440-sunsetMin) * 90;
+  }
+  else {
+    currentDegree = now/sunriseMin * 90;
+  }
+}
+
+function deg2Rad(degree) {
+  return Math.PI / 180 * degree; 
+}
